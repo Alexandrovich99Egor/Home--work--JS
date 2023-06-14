9e6be4aa36f8a3798a01162fed1d7505de01090e
Egor Alexandrovich <bbff33333@gmail.com>
2023-06-14 19:37:48 +0300

    14.05

--- Js--HomeWork-04/main.js
+++ Js--HomeWork-04/main.js
@@ -1,1 +1,14 @@
-let btn = document.querySelector('123');
+let btn = document.querySelectorAll('.btn');
+console.log(btn);
+const colors = ['red', 'blue', 'orange', 'pink', 'purple', 'dark']
+
+for (let i = 0; i < 8; i++) {
+    btn[i].onclick = () => {
+        btn[i].classList.toggle(colors[i]);
+    }
+
+}
+
+
+
+
--- Test/main.js
+++ Test/main.js
@@ -1,24 +1,3 @@
-function playGame(userChoice) {
-    var choices = ["камень", "ножницы", "бумага"];
-    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
-    var result;
-    if (userChoice === computerChoice) {
-        result = "Ничья!";
-    } else if (
-        (userChoice === "камень" && computerChoice === "ножницы") ||
-        (userChoice === "scissors" && computerChoice === "paper") ||
-        (userChoice === "paper" && computerChoice === "rock")
-    ) {
-        result = "Вы победили!";
-    } else {
-        result = "Вы проиграли!";
-    }
-
-    document.getElementById("result").textContent = `Ваш выбор: ${userChoice}. Выбор компьютера: ${computerChoice}. ${result}`;
-}
-
-
 
 
 
